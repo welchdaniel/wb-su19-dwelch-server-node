c23731d7c4dad926d9def712aef8b936945fa11e
welchdaniel <welch.da@husky.neu.edu>
2019-06-25 13:28:11 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var app = express();
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin",
+        "http://localhost:4200");
+    res.header("Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Methods",
+        "GET, POST, PUT, DELETE, OPTIONS");
+    res.header("Access-Control-Allow-Credentials", "true");
+    next();
+ }); 
+
+app.get('/hello', function(req, res){
+  res.send('hello world');
+});
+
+app.listen(3003);

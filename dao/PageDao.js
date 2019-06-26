b03bb566601c5b360173295403fa232b6e035b56
welchdaniel <welch.da@husky.neu.edu>
2019-06-25 22:24:24 -0400

    implemented pageDao using websiteModel

--- dao/PageDao.js
+++ dao/PageDao.js
@@ -1,23 +1,35 @@
 const pageModel = require('../models/PageModel');
+const websiteModel = require('../models/WebsiteModel')
 
 createPage = (websiteId, page) => {
-    //return pageModel.create(...);
+    websiteModel.update({_id: websiteId},{$push: {pages: page}});
 }
 
 findPageById = (websiteId, pageId) => {
-    //return pageModel.findById(...); 
+    websiteModel.findById(websiteId)
+       then(website => website.pages.id(pageId));
 }
 
 findAllPages = (websiteId) => {
-
+    websiteModel.findById(websiteId)
+       then(website => website.pages);
 }
 
 deletePage = (websiteId, pageId) => {
-    //return pageModel.findByIdAndDelete(...);
+    websiteModel.update(
+        {_id: websiteId},
+        {$pull: {pages: {_id: pageId}}}
+    )
 }
 
 updatePage = (websiteId, pageId, page) => {
-    //return pageModel.updateOne({_id: id}, {$set: page});
+    websiteModel.update(
+        {_id: websiteId, 'pages._id': pageId},
+        {$set: {
+            'pages.$.title': page.title,
+            'pages.$.rows': page.rows
+        }}
+    )
 }
 
 module.exports = {
